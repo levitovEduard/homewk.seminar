6d065ea71952504dce0675baaa6a8b937d6aed06
Eduard <levitov.eduard@gmail.com>
2022-12-04 00:22:42 +0200

    ustal ochen

---
+++ Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Hello, 21 World!");
--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int num1, num2 ;
+num1 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToInt32(Console.ReadLine());
+if ( num1> num2) 
+Console.WriteLine ("max{0}, min{1}", num1,num2);
+else 
+Console.WriteLine ("max {0}, min {1}", num1,num2); 
